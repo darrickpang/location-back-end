9f5a969682f4add85fb9a75d2d3f5720a9790b17
darrickpang <dkypang@yahoo.com>
2021-01-26 15:46:40 -0800

    friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
     before_action :authorized, only: [:persist]
+    def index
+        users = User.all 
+        render json: users 
+    end
+
     def new
         user = User.new
     end
