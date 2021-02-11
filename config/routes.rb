166f778a27e5f7750edbb9d8ff46a7f923b69242
darrickpang <dkypang@yahoo.com>
2021-02-10 18:28:06 -0800

    added stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :trips do
+    resources :checkins, only: :create
+  end
+  root 'trips#index'
   get 'checkins/create'
   get 'trips/index'
   get 'trips/create'
